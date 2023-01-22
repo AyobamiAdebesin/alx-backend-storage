9652b52509935afbcc46cfaf475641ed4f2fafea
AyobamiAdebesin <froshofficial@gmail.com>
2023-01-22 17:37:18 +0100

    Refactor: included ame of table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Creates a table called `users` with attributes
 -- `id`, `email`, `name`
-CREATE TABLE IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
