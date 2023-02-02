076fe1dd9d6f118aeed1979744ccdca93adeccc3
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-02 22:22:56 +0100

    Rearranged order of constraint

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- A script that creates a table users with
 -- attributes id, email, name and country
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') DEFAULT='US' NOT NULL
