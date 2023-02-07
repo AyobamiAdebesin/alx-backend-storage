976f390e53606dfc4c37749cd4ec2db128f393ee
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-07 03:18:50 +0100

    Added backticks

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- This SQL script select band_name, and lifespan column which is difference
-SELECT DISTINCT band_name, (IFNULL(split, 2020) - formed) as lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', style) > 0
-    ORDER BY lifespan DESC;
+SELECT DISTINCT `band_name`,
+                IFNULL(`split`, 2020) - `formed` as `lifespan`
+  FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
+  ORDER BY `lifespan` DESC;
