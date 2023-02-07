cabb567b3007fcc7af0304d62ab0f751a60673c8
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-07 02:54:22 +0100

    Moved things around

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,6 +2,6 @@
 -- their main style, ranked by their longetivity
 -- longetivity as lifespan = split - formed
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-ORDER BY lifespan DESC;
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
