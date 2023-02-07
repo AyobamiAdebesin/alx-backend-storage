971470d0065c2021db9911f78b97e22b1f5d8f15
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-07 03:01:38 +0100

    Adding the distinct constraint

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- This SQL script select band_name, and lifespan column which is difference
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+SELECT DISTINCT band_name, (IFNULL(split, 2020) - formed) as lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', style) > 0
     ORDER BY lifespan DESC;
