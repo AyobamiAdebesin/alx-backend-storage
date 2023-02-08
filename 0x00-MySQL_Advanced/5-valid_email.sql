5801b1e784dd35059a191f43b7dc86f08c64e30c
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-08 22:21:46 +0100

    Validating users with triggers

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- A sql script that creates a trigger that resets the attribute
+-- valid_email only when the email has been changed
+
+CREATE TRIGGER reset_attribute
+AFTER UPDATE ON users
+FOR EACH ROW
+	BEGIN
+		IF NEW.email != OLD.email THEN
+			SET NEW.valid_email = 1
+		END IF;
+	END;
