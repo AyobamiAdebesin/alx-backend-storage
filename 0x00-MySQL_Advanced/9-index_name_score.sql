fccbd6e9e55d17d0cc4291eaf5dd6a6b1e81f70d
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-10 00:01:46 +0100

    Modifying condition for score column

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- A SQL script that creats an index idx_name_first_score
 -- on the table `names` and the first letter of name and score
 
-CREATE INDEX IF NOT EXISTS idx_name_first_score ON names (name(1), score);
+CREATE INDEX IF NOT EXISTS idx_name_first_score ON names (name(1), score < 80);
