64c8235e006bb52c1384442f85a37a3e59ed41cb
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-10 08:48:26 +0100

    Completed task 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,16 +2,17 @@
 -- the first by the second number or returns 0 if the second number is 
 -- equal to 0
 
+DELIMITER |
 DROP FUNCTION IF EXISTS SafeDiv;
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT
-DETERMINISTIC
 BEGIN
-	DECLARE c FLOAT;
-	IF b = 0 THEN
-		SET c = 0
-	ELSE
-		SET c = a/ b;
-	END IF
-	RETURN c;
+  DECLARE result FLOAT;
+  IF b = 0 THEN
+    SET result = 0;
+  ELSE
+    SET result = a / b;
+  END IF;
+  RETURN result;
 END;
+|
