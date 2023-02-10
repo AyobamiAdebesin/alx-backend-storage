24d61d4d5f10de7f65990b80a091bc5bfbf275c8
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-10 08:24:57 +0100

    Retry

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,10 +2,12 @@
 -- the first by the second number or returns 0 if the second number is 
 -- equal to 0
 
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
 DETERMINISTIC
 BEGIN
-	DECLARE c INT;
+	DECLARE c FLOAT;
 	IF b = 0 THEN
 		SET c = 0
 	ELSE
