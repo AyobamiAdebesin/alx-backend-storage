71f78f724b9d7154751f559bf670ff068b132d58
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-10 22:17:53 +0100

    Updating documents in a collection

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" A function that changes all topics of a school document based
+on the name """
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on
+    the name
+    """
+    return mongo_colection.update_many(
+            {
+                "name": name
+                },
+            {"$set": {"topics": topics}
+                })
