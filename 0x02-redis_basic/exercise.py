02e2787ce854ab2dbbbaa43af7d8e3a1be79ea88
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-12 17:36:40 +0100

    First attempt at redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/ussr/bin/env python3
+""" Creating a Cache class """
+import redis
+from typing import List, Float, Union
+
+
+class Cache:
+    """ A Cache class """
+    def __init__(self):
+        """ Instantiate a Redis cache client """
+        self._redis = redis.Redis(host="localhost", port=6379, db=0)
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Takes data as an argument, generate a random key,
+        set the data as the value of the key and return the key
+        """
+        rand_key = str(uuid4())
+        self._redis.set(key, data)
+        return rand_key
